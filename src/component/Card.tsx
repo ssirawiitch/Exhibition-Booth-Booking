ff70da80fc6c3de491a2bd7064a69d4226b517b7
ssirawiitch <sirawitchchairuangsirikul@gmail.com>
2025-11-10 10:26:31 +0700

    fix: remove forgot password

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -67,7 +67,7 @@ export default function Card({
                     <span className="text-sm">{venue}</span>
                 </div>
                 
-                {/* <div className="flex items-center space-x-2 text-gray-600">
+                {/* <div className="flex items-center space-x-2 text-gray-600"> 
                     <Calendar className="w-5 h-5 text-red-600 flex-shrink-0" />
                     <span className="text-sm">
                         {formatStartDate(startDate)} - {calculateEndDate(startDate, durationDay)}
