905e07d8c4c1b33b00802e26c7e2c897fb6cd5cf
ssirawiitch <sirawitchchairuangsirikul@gmail.com>
2025-11-14 15:54:46 +0700

    fix path backend fetch

--- src/libs/editExhibition.tsx
+++ src/libs/editExhibition.tsx
@@ -1,6 +1,6 @@
 export default async function editExhibition(token: string, id: string , updatedData: any) {
 
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/exhibitions/${id}`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/exhibitions/${id}`, {
         method: "PUT",
         headers: {
             "Content-Type": "application/json",
--- src/libs/getExhibitions.tsx
+++ src/libs/getExhibitions.tsx
@@ -1,5 +1,5 @@
 export default async function getExhibitions() {
-    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/exhibitions`)
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/exhibitions`)
 
     if(!response.ok){
         throw new Error("Failed to fetch exhibitions")
