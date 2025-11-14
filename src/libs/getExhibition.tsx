36a42514de9ac6c8bfeba1729ee62cf136156a2d
Nykhanomjan <siwarat.ajar@gmail.com>
2025-11-14 18:15:59 +0700

    edit cach

--- src/libs/getExhibition.tsx
+++ src/libs/getExhibition.tsx
@@ -1,8 +1,11 @@
-export default async function getExhibition(id:string) {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/exhibitions/${id}`)
-    if(!response.ok){
-        throw new Error("Failed to fetch cars")
-    }
+export default async function getExhibition(id: string) {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/exhibitions/${id}`,
+    { cache: "no-store" }
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch cars");
+  }
 
-    return await response.json()
+  return await response.json();
 }
